109927ae61559af12a2aeb65cbe1b3c6caedb868
ji1xcn <44162150+ji1xcn@users.noreply.github.com>
2021-12-22 03:47:51 +0900

    fix

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -1,1 +1,5 @@
-eval "$(/opt/homebrew/bin/brew shellenv)
+if [ "$(uname -m)" == 'arm64' ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)
+elif [ "$(uname -m)" == 'x86_64' ]; then
+  eval "$(/usr/local/bin/brew shellenv)
+fi
