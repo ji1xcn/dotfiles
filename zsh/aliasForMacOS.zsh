5354bafd765661371b40775a0dbc2d243d436a70
ji1xcn <44162150+ji1xcn@users.noreply.github.com>
2022-02-23 16:46:53 +0900

    fix

--- zsh/aliasForMacOS.zsh
+++ zsh/aliasForMacOS.zsh
@@ -4,3 +4,6 @@ function resetLaunchPad(){
 }
 alias resetLaunchPad=resetLaunchPad
 
+alias resetAudio='sudo killall coreaudiod'
+
+alias pip-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pip install -U"
